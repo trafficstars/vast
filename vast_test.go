68d33064fa18dc1a4033deec23cb40983463772f
Alex Neill <alex.neill@gmail.com>
2017-02-15 14:51:06 +0000

    Implemented URIString, cleaning up parsed URIs

--- vast_test.go
+++ vast_test.go
@@ -33,8 +33,8 @@ var _ = Describe("VAST", func() {
 					AdSystem: &AdSystem{Version: "1.0", Name: "Acudeo Compatible"},
 					AdTitle:  &AdTitle{Name: "VAST 2.0 Instream Test 1"},
 					Impressions: []Impression{
-						{URI: "http://myTrackingURL/impression"},
-						{ID: "foo", URI: "http://myTrackingURL/impression2"},
+						{URI: URIString("http://myTrackingURL/impression")},
+						{ID: "foo", URI: URIString("http://myTrackingURL/impression2")},
 					},
 					Creatives: []Creative{
 						{
@@ -42,16 +42,16 @@ var _ = Describe("VAST", func() {
 							Linear: &Linear{
 								Duration: durationPtr(30 * time.Second),
 								TrackingEvents: []Tracking{
-									{Event: "creativeView", URI: "http://myTrackingURL/creativeView"},
-									{Event: "start", URI: "http://myTrackingURL/start"},
-									{Event: "midpoint", URI: "http://myTrackingURL/midpoint"},
-									{Event: "firstQuartile", URI: "http://myTrackingURL/firstQuartile"},
-									{Event: "thirdQuartile", URI: "http://myTrackingURL/thirdQuartile"},
-									{Event: "complete", URI: "http://myTrackingURL/complete"},
+									{Event: "creativeView", URI: URIString("http://myTrackingURL/creativeView")},
+									{Event: "start", URI: URIString("http://myTrackingURL/start")},
+									{Event: "midpoint", URI: URIString("http://myTrackingURL/midpoint")},
+									{Event: "firstQuartile", URI: URIString("http://myTrackingURL/firstQuartile")},
+									{Event: "thirdQuartile", URI: URIString("http://myTrackingURL/thirdQuartile")},
+									{Event: "complete", URI: URIString("http://myTrackingURL/complete")},
 								},
 								VideoClicks: &VideoClicks{
-									ClickThroughs:  []VideoClick{{URI: "http://www.tremormedia.com"}},
-									ClickTrackings: []VideoClick{{URI: "http://myTrackingURL/click"}},
+									ClickThroughs:  []VideoClick{{URI: URIString("http://www.tremormedia.com")}},
+									ClickTrackings: []VideoClick{{URI: URIString("http://myTrackingURL/click")}},
 								},
 								MediaFiles: []MediaFile{
 									{
@@ -62,7 +62,7 @@ var _ = Describe("VAST", func() {
 										Height:              300,
 										Scalable:            true,
 										MaintainAspectRatio: true,
-										URI:                 "http://cdnp.tremormedia.com/video/acudeo/Carrot_400x300_500kb.flv",
+										URI:                 URIString("http://cdnp.tremormedia.com/video/acudeo/Carrot_400x300_500kb.flv"),
 									},
 								},
 							},
@@ -75,17 +75,17 @@ var _ = Describe("VAST", func() {
 									{
 										Width:                 300,
 										Height:                250,
-										CompanionClickThrough: &CompanionClickThrough{URI: "http://www.tremormedia.com"},
-										StaticResource:        &StaticResource{CreativeType: "image/jpeg", URI: "http://demo.tremormedia.com/proddev/vast/Blistex1.jpg"},
+										CompanionClickThrough: &CompanionClickThrough{URI: URIString("http://www.tremormedia.com")},
+										StaticResource:        &StaticResource{CreativeType: "image/jpeg", URI: URIString("http://demo.tremormedia.com/proddev/vast/Blistex1.jpg")},
 										TrackingEvents: []Tracking{
-											{Event: "creativeView", URI: "http://myTrackingURL/firstCompanionCreativeView"},
+											{Event: "creativeView", URI: URIString("http://myTrackingURL/firstCompanionCreativeView")},
 										},
 									},
 									{
 										Width:                 728,
 										Height:                90,
-										CompanionClickThrough: &CompanionClickThrough{URI: "http://www.tremormedia.com"},
-										StaticResource:        &StaticResource{CreativeType: "image/jpeg", URI: "http://demo.tremormedia.com/proddev/vast/728x90_banner1.jpg"},
+										CompanionClickThrough: &CompanionClickThrough{URI: URIString("http://www.tremormedia.com")},
+										StaticResource:        &StaticResource{CreativeType: "image/jpeg", URI: URIString("http://demo.tremormedia.com/proddev/vast/728x90_banner1.jpg")},
 									},
 								},
 							},
@@ -93,8 +93,8 @@ var _ = Describe("VAST", func() {
 					},
 					Description: "VAST 2.0 Instream Test 1",
 					Error: []Error{
-						{URI: "http://myErrorURL/error"},
-						{URI: "http://myErrorURL/error2"},
+						{URI: URIString("http://myErrorURL/error")},
+						{URI: URIString("http://myErrorURL/error2")},
 					},
 				},
 			}},
@@ -110,18 +110,18 @@ var _ = Describe("VAST", func() {
 						AdTitle:     &AdTitle{Name: "NonLinear Test Campaign 1"},
 						Description: "NonLinear Test Campaign 1",
 						Survey:      "http://mySurveyURL/survey",
-						Error:       []Error{{URI: "http://myErrorURL/error"}},
-						Impressions: []Impression{{URI: "http://myTrackingURL/impression"}},
+						Error:       []Error{{URI: URIString("http://myErrorURL/error")}},
+						Impressions: []Impression{{URI: URIString("http://myTrackingURL/impression")}},
 						Creatives: []Creative{
 							{
 								AdID: "602678-NonLinear",
 								NonLinearAds: &NonLinearAds{
 									TrackingEvents: []Tracking{
-										{Event: "creativeView", URI: "http://myTrackingURL/nonlinear/creativeView"},
-										{Event: "expand", URI: "http://myTrackingURL/nonlinear/expand"},
-										{Event: "collapse", URI: "http://myTrackingURL/nonlinear/collapse"},
-										{Event: "acceptInvitation", URI: "http://myTrackingURL/nonlinear/acceptInvitation"},
-										{Event: "close", URI: "http://myTrackingURL/nonlinear/close"},
+										{Event: "creativeView", URI: URIString("http://myTrackingURL/nonlinear/creativeView")},
+										{Event: "expand", URI: URIString("http://myTrackingURL/nonlinear/expand")},
+										{Event: "collapse", URI: URIString("http://myTrackingURL/nonlinear/collapse")},
+										{Event: "acceptInvitation", URI: URIString("http://myTrackingURL/nonlinear/acceptInvitation")},
+										{Event: "close", URI: URIString("http://myTrackingURL/nonlinear/close")},
 									},
 									NonLinears: []NonLinear{
 										{
@@ -130,8 +130,7 @@ var _ = Describe("VAST", func() {
 											MinSuggestedDuration: durationPtr(15 * time.Second),
 											StaticResource: &StaticResource{
 												CreativeType: "image/jpeg",
-												// TODO: Normalize these URIs
-												URI: "\n\t\t\t\t\thttp://demo.tremormedia.com/proddev/vast/50x300_static.jpg\n\t\t\t\t\t",
+												URI:          URIString("http://demo.tremormedia.com/proddev/vast/50x300_static.jpg"),
 											},
 											NonLinearClickThrough: "http://www.tremormedia.com",
 										},
@@ -141,8 +140,7 @@ var _ = Describe("VAST", func() {
 											MinSuggestedDuration: durationPtr(20 * time.Second),
 											StaticResource: &StaticResource{
 												CreativeType: "image/jpeg",
-												// TODO: Normalize these URIs
-												URI: "\n\t\t\t\t\thttp://demo.tremormedia.com/proddev/vast/50x450_static.jpg\n\t\t\t\t\t",
+												URI:          URIString("http://demo.tremormedia.com/proddev/vast/50x450_static.jpg"),
 											},
 											NonLinearClickThrough: "http://www.tremormedia.com",
 										},
@@ -158,21 +156,21 @@ var _ = Describe("VAST", func() {
 											Height: 250,
 											StaticResource: &StaticResource{
 												CreativeType: "application/x-shockwave-flash",
-												URI:          "http://demo.tremormedia.com/proddev/vast/300x250_companion_1.swf",
+												URI:          URIString("http://demo.tremormedia.com/proddev/vast/300x250_companion_1.swf"),
 											},
-											CompanionClickThrough: &CompanionClickThrough{URI: "http://www.tremormedia.com"},
+											CompanionClickThrough: &CompanionClickThrough{URI: URIString("http://www.tremormedia.com")},
 										},
 										{
 											Width:  728,
 											Height: 90,
 											StaticResource: &StaticResource{
 												CreativeType: "image/jpeg",
-												URI:          "http://demo.tremormedia.com/proddev/vast/728x90_banner1.jpg",
+												URI:          URIString("http://demo.tremormedia.com/proddev/vast/728x90_banner1.jpg"),
 											},
 											TrackingEvents: []Tracking{
-												{Event: "creativeView", URI: "http://myTrackingURL/secondCompanion"},
+												{Event: "creativeView", URI: URIString("http://myTrackingURL/secondCompanion")},
 											},
-											CompanionClickThrough: &CompanionClickThrough{URI: "http://www.tremormedia.com"},
+											CompanionClickThrough: &CompanionClickThrough{URI: URIString("http://www.tremormedia.com")},
 										},
 									},
 								},
